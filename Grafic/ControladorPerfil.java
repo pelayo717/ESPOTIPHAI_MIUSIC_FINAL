f131f308976f8e377fe6a28b3a0c2b1a2d19d8ea
ManuelSalvador3 <manusalvadorg@gmail.com>
2019-06-10 16:44:53 +0200

    Update ControladorPerfil.java

--- Grafic/ControladorPerfil.java
+++ Grafic/ControladorPerfil.java
@@ -8,16 +8,31 @@ import javax.swing.*;
 
 import ESPOTIPHAI_MIUSIC_FINAL.com.ESPOTIPHAI_MIUSIC.sistema.Sistema;
 
+/**
+ * Clase ControladorPerfil la cual describe todos las posibles
+ * acciones que el usuario puede realizar dentro de vista
+ */
 public class ControladorPerfil implements ActionListener{
 		private Perfil vista;
 		private int modelo;
 		
-		
+		/**
+		 * Contructor de la clase ControladorPerfil donde le asignamos los valores determinados 
+		 * a los atributos de la clase
+	 	 * @param vista: parametro de tipo perfil que indica en que vista se esta actualmente
+	 	 * @param modelo: parametro de tipo int que nos indica en que modelo nos encontramos
+	 	 */
 		public ControladorPerfil(Perfil vista, int modelo) {
 			this.vista = vista;
 			this.modelo = modelo;
 		}
 	 
+		/**
+	 	 * Funcion que determina que accion va a realizar el sistema 
+		 * dependiendo de los que pulse el usuario
+	 	 * @param e: parametro que indica la accion que ha realizado
+		 * el usuario
+	 	 */
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (((JButton)e.getSource()).getText() == "Inicio") {
