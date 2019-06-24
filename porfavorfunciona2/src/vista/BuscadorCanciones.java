a4b7416cd9a0050a74648342a358328299ac8c71
ManuelSalvador3 <manusalvadorg@gmail.com>
2019-06-24 12:36:44 +0200

    Update BuscadorCanciones.java

--- porfavorfunciona2/src/vista/BuscadorCanciones.java
+++ porfavorfunciona2/src/vista/BuscadorCanciones.java
@@ -14,11 +14,12 @@ import javax.swing.SwingConstants;
 
 import modelo.contenido.Cancion;
 
+/**
+ * Clase en la que se implementa la vistaBuscadorCanciones con todo
+ * lo necesario para cumplir los requisitos impuestos
+ */
 public class BuscadorCanciones extends PantallaPrincipal{ //99.9% esta terminado
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	private  DefaultListModel<String> model1;
@@ -33,6 +34,11 @@ public class BuscadorCanciones extends PantallaPrincipal{ //99.9% esta terminado
 	
 	public JButton seleccionarCancion;
 	
+	
+	/**
+	 * Constructor de la clase BuscadorCanciones donde se inicializan
+	 * todos los atributos con lo valores correspondientes 
+	 */
 	public BuscadorCanciones() {
 		
 		super();
@@ -79,6 +85,11 @@ public class BuscadorCanciones extends PantallaPrincipal{ //99.9% esta terminado
 		
 	}
 
+
+	/**
+	 * Funcion que asgina a cada boton la accion que se pasa como argumento
+	 * @param c: accion que se va a pasar a cada boton para que luego sse asigne el usuario determinado
+	 */
 	public void setControlador(ActionListener c) {
 		super.getBotonIzquierdaArriba().addActionListener(c);
 		super.getBotonIzquierdaMedio().addActionListener(c);
@@ -88,46 +99,89 @@ public class BuscadorCanciones extends PantallaPrincipal{ //99.9% esta terminado
 		super.getBotonLimpiarBuscador().addActionListener(c);
 	}
 	
+	
+	/**
+	 * Funcion que cambia el texto de unos determinados botones para ponerlos el 
+	 * texto a lo necesario para cuando el usuario si que esa registrado
+	 */
 	public void setUsuarioRegistrado() {
 		super.getBotonIzquierdaArriba().setText("Ver Perfil");
 		super.getBotonIzquierdaMedio().setText("Inicio");
 		super.getBotonIzquierdaAbajo().setVisible(false);
 	}
-			
+		
+	
+	/**
+	 * Funcion que cambia el texto de unos determinados botones para ponerlos el 
+	 * texto a lo necesario para cuando el usuario no esta registrado
+	 */
 	public void setUsuarioNoRegistrado() {
 		super.getBotonIzquierdaArriba().setText("Iniciar Sesion");
 		super.getBotonIzquierdaMedio().setText("Registro");
 		super.getBotonIzquierdaAbajo().setVisible(true);
 	}
 	
+	/**
+	 * Funcion que delvuelve el atributo serialVersionUID de la clase
+	 * @param serialVersionUID:
+	 */
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
+	/**
+	 * Funcion que devuelve el modelo1 de la clase
+	 * @param model1: atributo que indica que estamos en el modelo 1
+	 */
 	public DefaultListModel<String> getModel1() {
 		return model1;
 	}
 
+	/**
+	 * Funcion que devuelve un array con todos las canciones que hay 
+	 * @return lasCanciones: array de albumes que contiene todos los albumes
+	 */
 	public Cancion[] getLasCanciones() {
 		return lasCanciones;
 	}
 
+	/**
+	 * Funcion que devuelve una lista de canciones
+	 * @return lista_canciones: lista que contiene todos las canciones 
+	 */
 	public JList<String> getLista_canciones() {
 		return lista_canciones;
 	}
 
+	/**
+	 * Funcion que devuelve un JScrollPane con todos las canciones
+	 * @return canciones: JScrollPane en el que dentro esta la lista de canciones
+	 */
 	public JScrollPane getCanciones() {
 		return canciones;
 	}
 
+	/**
+	 * Funcion que devuelve las canciones despues de la busqueda
+	 * @param cancionesEncontradas: JLabel que contiene todos las canciones 
+	 * encontrados despues de la busqueda
+	 */
 	public JLabel getCancionesEncontradas() {
 		return cancionesEncontradas;
 	}
 
+	/**
+	 * Funcion que devuelve el boton de seleccionar cancion
+	 * @return seleccionarCancion: devuelve el boton de seleccionarCancion
+	 */
 	public JButton getSeleccionarCancion() {
 		return seleccionarCancion;
 	}
 
+	/**
+	 * Funcion que actualiza las canciones por el arrat que se le pasa
+	 * @param canciones_propias: canciones que se van a poner como nuevas al actualizar
+	 */
 	public void actualizarCanciones(Cancion[] canciones_propias) {
 		model1.clear();
 		lasCanciones = canciones_propias;
@@ -141,6 +195,9 @@ public class BuscadorCanciones extends PantallaPrincipal{ //99.9% esta terminado
 		}
 	}
 	
+	/**
+	 * Funcion que limpia los datos completamente
+	 */
 	public void limpiarDatos() {
 		model1.clear();
 	}
